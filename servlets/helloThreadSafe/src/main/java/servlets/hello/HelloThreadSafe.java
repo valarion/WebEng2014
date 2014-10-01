0b74487c5e50ce246006ff7e31dfffb01abb320f
Francisco J. Lopez-Pellicer <fjlopez@unizar.es>
2014-10-02 01:00:42 +0200

    Rename Singleton to ThreadSafe

--- servlets/helloThreadSafe/src/main/java/servlets/hello/HelloThreadSafe.java
+++ servlets/helloThreadSafe/src/main/java/servlets/hello/HelloThreadSafe.java
@@ -12,18 +12,18 @@ import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = { "/hi" })
-public class HelloSingleton extends HttpServlet {
+public class HelloThreadSafe extends HttpServlet {
 
-	private AtomicInteger ticks = new AtomicInteger();
+	private AtomicInteger thisIsNotThreadSafe = new AtomicInteger();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String name = req.getParameter("name");
+		String thisIsThreadSafe = req.getParameter("name");
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
-		out.println("<html><head><title>Hello " + name + "!</title></head>"
-				+ "<body><h1>Hello " + name + " (" + ticks.incrementAndGet()
+		out.println("<html><head><title>Hello " + thisIsThreadSafe + "!</title></head>"
+				+ "<body><h1>Hello " + thisIsThreadSafe + " (" + thisIsNotThreadSafe.incrementAndGet()
 				+ " times)!</h1></body></html>");
 	}
 
